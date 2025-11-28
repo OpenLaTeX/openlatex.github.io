fafd16658d84e9a6a5065c461f3cfb654ce3949c
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2025-11-28 22:52:48 +0100

    samesite none

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -64,7 +64,7 @@ router.post('/login', async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax'
+            sameSite: 'none'
         });
 
         res.json({ token });
