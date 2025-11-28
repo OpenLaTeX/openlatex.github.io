c4fe9d1c6de8f32c837389a6770cbc42d137b8ef
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2025-11-28 22:31:59 +0100

    fix samesite

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -64,7 +64,7 @@ router.post('/login', async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict'
+            sameSite: 'lax'
         });
 
         res.json({ token });
