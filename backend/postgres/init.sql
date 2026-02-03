2b2c1d3807b6e238d103b197763635b7277679df
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-02-03 18:46:40 +0100

    sec(db): Random UUID for primary keys

--- backend/postgres/init.sql
+++ backend/postgres/init.sql
@@ -1,23 +1,25 @@
 --- conventions nommages | syntaxe de l'IUTINFO Villeneuve d'ascq
 
+create extension if not exists pgcrypto;
+
 create table if not exists users (
-    uno serial primary key,
+    uno text primary key default gen_random_uuid()::text,
     email text unique not null,
     password text not null,
     created_at timestamp default now()
 );
 
 create table if not exists projects (
-    pno serial primary key,
-    uno int not null references users(uno) on delete cascade,
+    pno text primary key default gen_random_uuid()::text,
+    uno text not null references users(uno) on delete cascade,
     name text not null,
     description text,
     created_at timestamp default now()
 );
 
 create table if not exists files (
-    fno serial primary key,
-    pno int not null references projects(pno) on delete cascade,
+    fno text primary key default gen_random_uuid()::text,
+    pno text not null references projects(pno) on delete cascade,
     filename text not null,
     content bytea not null,
     file_type text not null,
