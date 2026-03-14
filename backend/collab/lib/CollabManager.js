385f2d2f58e37f05a9d0d3344f6e1338f6b7ba59
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-03-14 22:02:45 +0100

    feat(yjs): binary content supported

--- backend/collab/lib/CollabManager.js
+++ backend/collab/lib/CollabManager.js
@@ -31,12 +31,17 @@ setPersistence({
                 [docName]
             );
             const filesMap = ydoc.getMap('files');
+            const filesMeta = ydoc.getMap('filesMeta');
             for (const file of filesResult.rows) {
-                const isBinary = ['png', 'jpg', 'pdf'].includes(file.file_type);
-                if (!isBinary) {
+                const isBinary = ['png', 'jpg', 'jpeg', 'pdf'].includes(file.file_type);
+                if (isBinary) {
+                    const base64 = file.content.toString('base64');
+                    filesMeta.set(file.filename, JSON.stringify({ type: file.file_type, content: base64 }));
+                } else {
                     const yText = new Y.Text();
                     yText.insert(0, file.content.toString('utf-8'));
                     filesMap.set(file.filename, yText);
+                    filesMeta.set(file.filename, JSON.stringify({ type: file.file_type }));
                 }
             }
         }
