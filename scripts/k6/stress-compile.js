2b8e78143a3049c00867fbae8444e500a02cbb7f
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-03-15 10:43:39 +0100

    perf(compilation): testing 3 as max concurrent

--- scripts/k6/stress-compile.js
+++ scripts/k6/stress-compile.js
@@ -16,8 +16,8 @@ export const options = {
   scenarios: {
     stress_compile: {
       executor: 'shared-iterations',
-      vus: 2,
-      iterations: parseInt(__ENV.BURST_RATE || '80', 10),
+      vus: 1,
+      iterations: parseInt(__ENV.BURST_RATE || '200', 10),
       maxDuration: '4m',
     },
   },
