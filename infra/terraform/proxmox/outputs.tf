56b454cbcb4048bd207832773be12e6028373c52
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-05-02 21:53:42 +0200

    feat(ci, proxmox): Testing self-hosted runner

--- infra/terraform/proxmox/outputs.tf
+++ infra/terraform/proxmox/outputs.tf
@@ -1,8 +1,8 @@
-output "ssh_vm" {
-  description = "Commande SSH vers la vm testtt"
-  value       = "ssh -i ~/.ssh/github_deploy_key debian@${proxmox_virtual_environment_vm.debian.ipv4_addresses[1][0]}"
+output "openlatex_runner_ssh_vm" {
+  description = "Commande SSH vers la vm Runner"
+  value       = "ssh -i ~/.ssh/github_deploy_key debian@${proxmox_virtual_environment_vm.openlatex-runner.ipv4_addresses[1][0]}"
 }
 
-output "debian_ip" {
-  value = proxmox_virtual_environment_vm.debian.ipv4_addresses
+output "openlatex_runner_ip" {
+  value = proxmox_virtual_environment_vm.openlatex-runner.ipv4_addresses
 }
---
+++ infra/terraform/proxmox/runner.tf
@@ -0,0 +1,58 @@
+resource "proxmox_virtual_environment_vm" "openlatex-runner" {
+  name      = "openlatex-runner"
+  node_name = "homelab"
+
+  cpu {
+    cores = 1
+  }
+
+  memory {
+    dedicated = 1024
+  }
+
+  clone {
+    vm_id = 9000
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 20
+  }
+
+  agent {
+    enabled = true
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      keys     = [file(var.ssh_public_key_path)]
+      username = "debian"
+    }
+    user_data_file_id = proxmox_virtual_environment_file.user_data.id
+  }
+}
+
+resource "proxmox_virtual_environment_file" "user_data" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = "homelab"
+
+  source_raw {
+    file_name = "user-data.yaml"
+    data = templatefile("${path.module}/cloud-init/user-data-runner.sh.tpl", {
+      ssh_public_key_path = file(var.ssh_public_key_path)
+      runner_token        = var.runner_token
+    })
+  }
+}
