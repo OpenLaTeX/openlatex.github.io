e64d6158e091a6e4d988800df643c1ec2fd42bba
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-07-22 15:02:27 +0200

    deploy(compose only): Gen env + meilleure gestion par défaut pour le compose prod

--- deploy/curl/install-compose-standalone.sh
+++ deploy/curl/install-compose-standalone.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 git clone https://github.com/OpenLaTeX/openlatex.github.io openlatex
+cd openlatex
 git checkout 32-déploiement-compose-only-alternative-à-helm
-cd openlatex/deploy/compose
+cd deploy/compose
 
 # generation passwords
-[ -f .env ] || (POSTGRES_PASSWORD="$(openssl rand -hex 32)" && echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env) && (JWT_SECRET="$(openssl rand -hex 32)" && echo "JWT_SECRET=$JWT_SECRET" >> .env) && (TEST_BYPASS_SECRET="$(openssl rand -hex 32)" && echo "TEST_BYPASS_SECRET=$TEST_BYPASS_SECRET" >> .env)
+chmod +x gen-env.sh
+./gen-env.sh
+
 docker compose -f docker-compose.full-standalone.yml up -d
